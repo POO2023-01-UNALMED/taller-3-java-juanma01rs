349b30d2bbc508b1223efd150c064850f2b0e417
juanma01rs <juanma10rs@gmail.com>
2023-02-27 19:41:20 -0500

    Java 7

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -3,20 +3,17 @@ package taller3.televisores;
 public class TV {
 	
 	private Marca marca;
-	private static int canal;
-	private static int precio;
+	private int canal = 1;
+	private int precio = 500;
 	private boolean estado;
-	private static int volumen;
+	private int volumen = 1;
 	private Control control;
 	private static int numTV;
 	
 	public TV (Marca marca, boolean estado) {
 		this.marca = marca;
 		this.estado = estado;
-		canal = 1;
-		volumen = 1;
-		precio = 500;
-		numTV++;
+		TV.numTV++;
     }
 	public void setMarca (Marca marca) {
 		this.marca = marca;
